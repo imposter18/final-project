4b25a6a142bc27fa458822ebb924235099494906
ivan <redneckk@protonmail.com>
2020-11-11 20:42:14 +0300

    !!!

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,33 @@ $(document).on('DOMContentLoaded', () => {
     prevArrow: $('.prev'),
       nextArrow: $('.next'),
     });
+    $('.productCard__slider__one-main').slick({
+      slidesToShow: 1,
+      // prevArrow: $('.prev'),
+      // nextArrow: $('.next'),
+      fade: true,
+      centerMode: false,
+      arrows: false, 
+     
+    });
+    $('.productCard__slider__one-support').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: '.productCard__slider__one-main',
+      dots: false,
+      centerMode: false,
+      focusOnSelect: true,
+      arrows: false,  
+    });
+    $('#group input:checkbox').click(function(){
+      if ($(this).is(':checked')) {
+         $('#group input:checkbox').not(this).prop('checked', false);
+      }
+    });
+    $('#group-checkbox input:checkbox').click(function(){
+      if ($(this).is(':checked')) {
+         $('#group-checkbox input:checkbox').not(this).prop('checked', false);
+      }
+    });
 });
   
